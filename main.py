9a14acb4f72e59025b27d7c5da6a5a663df06b71
Bou Jia <boubasaur@gmail.com>
2022-08-23 15:27:27 -0700

    edit

--- main.py
+++ main.py
@@ -50,12 +50,18 @@ def add_password():
         messagebox.showinfo(title='Error', message='Please input all fields')
         
     else:
-        with open('data.json', 'r') as file:
-            data = json.load(file)
+        try:
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
             data.update(new_data)
-        with open('data.json', 'w') as file:
-            json.dump(data, file, indent=4)
 
+            with open('data.json', 'w') as file:
+                json.dump(data, file, indent=4)
+        finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
                 
